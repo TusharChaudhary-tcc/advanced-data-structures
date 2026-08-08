f490848d3c98649c247249333dbd3c429f82459c
TusharChaudhary-tcc <tusharchaudhary6160@gmail.com>
2026-08-08 22:58:09 +0530

    Program for Decimal to Binary Conversion

---
+++ 01_Stack/02_DecimalToBinary.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Stack{
+    vector<int> vec;
+    int top;
+
+    public:
+    Stack(int s){
+        vec.resize(s);
+        top=-1;
+    }
+
+    void Push(int x){
+        if(top==vec.size()-1){
+            cout<<"Stack Overflows"<<endl;
+            exit(1);
+        }
+        top++;
+        vec[top]=x;
+    }
+    int Pop(){
+        if(top==-1){
+            cout<<"Stack Underflows"<<endl;
+            exit(1);
+        }
+        int x=vec[top];
+        top--;
+        return x;
+    }
+    int StackTop(){
+        if(top==-1){
+            cout<<"Stack Underflows"<<endl;
+            exit(1);
+        }
+        int x=vec[top];
+        return x;
+    }
+
+    bool isEmpty(){
+        if(top==-1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
+    bool isFull(){
+        if(top==vec.size()-1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    int Size(){
+        return top+1;
+    }
+
+    void Display(){
+        int temp=top;
+        while(temp!=-1){
+            cout<<vec[temp]<<" ";
+            temp--;
+        }
+    }
+};
+
+int main(){
+    Stack st(20);
+    
+    int n;
+    cout<<"Enter a Decimal Number : ";
+    cin>>n;
+
+    while(n>0){
+        st.Push(n%2);
+        n/=2;
+    }
+    cout<<"Binary Form :- ";
+    while(!st.isEmpty()){
+        int a=st.StackTop();
+        st.Pop();
+        cout<<a;
+    }
+
+}
